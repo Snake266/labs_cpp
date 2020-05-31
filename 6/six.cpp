88c44bb7367a1db3ab388c814818df16f773f513
Georgii <georgij.baturin@gmail.com>
2020-05-31 11:13:24 +0200

    Перевел 6 лабу с std::vector на самописныйы container

--- 6/six.cpp
+++ 6/six.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <iomanip>
-#include <vector>
 #include <algorithm>
 #include <cstring>
 
@@ -58,25 +57,60 @@ struct parcel_t {
 
 };
 
+struct container {
+    parcel_t** array;
+    size_t size;
+    size_t capasity;
+
+};
+
+void _copy_parcel(parcel_t** dest, parcel_t** src, size_t n) {
+    memcpy(dest, src, n);
+}
+
+void push_back(container* c, parcel_t* p) {
+    if(c->array == NULL) {
+        c->capasity = 20;
+        c->size = 0;
+        c->array = new parcel_t*[c->capasity];
+    }
+    if(c->size == c->capasity) {
+        c->capasity *= 2;
+        parcel_t** tmp = new parcel_t*[c->capasity];
+        _copy_parcel(tmp, c->array, c->size);
+        delete[] c->array;
+        c->array = tmp;
+    }
+    c->size++;
+    c->array[c->size-1] = p;
+}
 
 //Функция, которая добавляет в конец коллекции новую поссылку
-void new_parcel(std::vector<parcel_t*>*);
+void new_parcel(container*);
 
 //Печатаем таблицу
 void print_parcel(parcel_t p);
 
-void print_table(std::vector<parcel_t*>& p);
+void print_table(container* p);
 
 //ищем поссылку по фамилии и имени отправителя
-void find_by_sender(std::vector<parcel_t*>&);
+void find_by_sender(container* p);
 
-void find_by_weight(std::vector<parcel_t*>&);
+void find_by_weight(container* p);
+
+container* init_container() {
+    container* c = new container;
+    c->array = NULL;
+    c->size = 0;
+    c->capasity = 0;
+    return c;
+}
 
 int main() {
     setlocale(LC_ALL, "Russian");
     //Будем использовать std::vector, чтобы не писать свой std::vector
     //вектор аллоцирует данные в динамической памяти, поэтому все по требованиям
-    std::vector<parcel_t*> parcels; 
+    container* parcels = init_container();
         char choice = '0';
     do {
         std::cout << "Что вы хотите сделать?" << std::endl;
@@ -88,7 +122,7 @@ int main() {
         switch (choice)
         {
         case '1':
-            new_parcel(&parcels);
+            new_parcel(parcels);
             break;
         case '2':
             print_table(parcels);
@@ -123,7 +157,7 @@ char* promt(const char* message) {
     return dest;
 }
 
-void new_parcel(std::vector<parcel_t*>* p) {
+void new_parcel(container* p) {
     std::cout << std::endl;
     for(int i = 0; i < sep_len; ++i) {
         std::cout << '-';
@@ -151,7 +185,7 @@ void new_parcel(std::vector<parcel_t*>* p) {
     std::cin >> weight;
     
     //Добавляем в конец массива объект
-    p->push_back(new parcel_t(s_last_name, s_first_name, s_adress, 
+    push_back(p, new parcel_t(s_last_name, s_first_name, s_adress, 
                         r_last_name, r_first_name, r_adress, weight));
 
     //Чистим память
@@ -183,21 +217,21 @@ void print_parcel(parcel_t* p) {
     std::cout << std::endl;
 }
 
-void print_table(std::vector<parcel_t*>& p) {
-    if(p.size() == 0) std::cout << "Поссылок нет" << std::endl;
+void print_table(container* p) {
+    if(p->size == 0) std::cout << "Поссылок нет" << std::endl;
     else {
         for(int i = 0; i < sep_len; ++i) {
             std::cout << '-';
         }
         std::cout << std::endl;
 
-        for(int i = 0; i < p.size(); ++i) {
-            print_parcel(p[i]);
+        for(int i = 0; i < p->size; ++i) {
+            print_parcel(p->array[i]);
         }
     }
 }
 
-void find_by_sender(std::vector<parcel_t*>& p) {
+void find_by_sender(container* p) {
     for(int i = 0; i < sep_len; ++i) std::cout << '-';
     std::cout << std::endl;
 
@@ -209,30 +243,45 @@ void find_by_sender(std::vector<parcel_t*>& p) {
     std::cin.getline(sender_fname, 80);
     
     //Проходим по всему массиву
-    for(auto& i : p) {
-        if(!strncmp(i->s_first_name, sender_fname, strlen(i->s_first_name)) 
-           && !strncmp(i->s_last_name, sender_lname, strlen(i->s_last_name))) {
-            print_parcel(i);
+    for(int i = 0; i < p->size; ++i) {
+        if(!strncmp(p->array[i]->s_first_name, sender_fname, strlen(p->array[i]->s_first_name)) 
+           && !strncmp(p->array[i]->s_last_name, sender_lname, strlen(p->array[i]->s_last_name))) {
+            print_parcel(p->array[i]);
         }
     }
     for(int i = 0; i < sep_len; ++i) std::cout << '-';
     std::cout << std::endl;
 }
 
-void find_by_weight(std::vector<parcel_t*>& p) {
+
+//Напишем сортировку вставками. Мы не знаем, данные могут быть почти отсортированными, могут таковыми не быть
+//но в задании нет ограничений, поэтому я хочу попробовать написать именно эту сортировку
+void insertion_sort(parcel_t** p, size_t size) {
+    parcel_t *tmp, *key;
+    int i, j;
+    for(i = 0; i < size; ++i) {
+        key = p[i];
+        for(j = i - 1; j >= 0 && strcmp(p[j]->s_last_name, key->s_last_name) > 0; --j) {
+            p[j+1] = p[j];
+        }
+        p[j+1] = key;
+    }
+}
+
+void find_by_weight(container* p) {
     for(int i = 0; i < sep_len; ++i) std::cout << '-';
     std::cout << std::endl;
     double w;
     std::cout << "Введите вес поссылки: ";
     std::cin >> w;
 
-    std::vector<parcel_t*> tmp;
+    container* tmp = init_container();
     
-    for(auto i : p) {
-        if(i->weight > w) tmp.push_back(i);
+    for(int i = 0; i < p->size; ++i) {
+        if(p->array[i]->weight > w) push_back(tmp, p->array[i]);
     }
 
-    std::sort(tmp.begin(), tmp.end(), [](parcel_t* a1, parcel_t* a2) {
+/*    std::sort(tmp.begin(), tmp.end(), [](parcel_t* a1, parcel_t* a2) {
         //Создадим временные строки, которые мы будем дальше сравнивать
         char* tmp1 = new char[strlen(a1->s_last_name) + strlen(a1->s_first_name) + 2];
         char* tmp2 = new char[strlen(a2->s_last_name) + strlen(a2->s_first_name) + 2];
@@ -253,7 +302,10 @@ void find_by_weight(std::vector<parcel_t*>& p) {
             delete[] tmp2;
             return false;
         } 
-    });
+    });*/
+
+    //сортировка
+    insertion_sort(tmp->array, tmp->size);
 
     print_table(tmp);
 
